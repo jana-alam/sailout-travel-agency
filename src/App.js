c32b1e5c00050584a80f7241b3e6f6a04dd10992
jana-alam <janaalam2929@gmail.com>
2021-10-29 08:32:49 +0600

    tailwind installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>This is header</h2>
+      <h2 className="text-7xl text-red-500">This is header</h2>
     </div>
   );
 }
