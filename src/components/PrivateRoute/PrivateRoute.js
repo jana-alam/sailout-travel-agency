a2c479e493411b1f49a2788426b8f91692720750
jana-alam <janaalam2929@gmail.com>
2021-10-29 15:03:06 +0600

    spinner added for reload on private route

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,26 +1,36 @@
 import React from "react";
 import { Redirect, Route } from "react-router";
 import useAuth from "../../hooks/useAuth";
+import { RefreshIcon } from "@heroicons/react/outline";
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const { user } = useAuth();
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        user?.email ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: location },
-            }}
-          />
-        )
-      }
-    ></Route>
-  );
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <div className="mx-auto w-12">
+        <RefreshIcon className="animate-spin text-yellow-400" />
+      </div>
+    );
+  } else {
+    return (
+      <Route
+        {...rest}
+        render={({ location }) =>
+          user?.email ? (
+            children
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location },
+              }}
+            />
+          )
+        }
+      ></Route>
+    );
+  }
 };
 
 export default PrivateRoute;
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -12,17 +12,20 @@ firebaseFire();
 
 const useFirebase = () => {
   const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const auth = getAuth();
   const provider = new GoogleAuthProvider();
 
   //   auth observer
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
+      setIsLoading(true);
       if (user?.email) {
         setUser(user);
       } else {
         setUser({});
       }
+      setIsLoading(false);
     });
   }, [auth]);
 
@@ -42,7 +45,7 @@ const useFirebase = () => {
     signOut(auth).then((result) => setUser({}));
   };
 
-  return { user, setUser, googleSignIn, logOut };
+  return { user, setUser, googleSignIn, logOut, isLoading };
 };
 
 export default useFirebase;
