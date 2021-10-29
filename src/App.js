3b95a06f9a2e86894e0a10e64695d898fbf4f582
jana-alam <janaalam2929@gmail.com>
2021-10-29 09:11:06 +0600

    Header implemented by installing headless react & heroincons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h2 className="text-7xl text-red-500">This is header</h2>
+      <Header></Header>
+      <h2 className="bg-red-500 py-4 text-center">Hello Tailwind!!!</h2>
+      <h2 className="bg-red-500 py-4 text-center">Hello Tailwind!!!</h2>
     </div>
   );
 }
