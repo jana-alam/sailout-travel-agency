5e421b831b2755067ce6a04215d8b9eb4a15caae
jana-alam <janaalam2929@gmail.com>
2021-10-31 00:39:52 +0600

    Login & notFound page done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,7 @@ import useAuth from "../../hooks/useAuth";
 
 const Login = () => {
   const { googleSignIn } = useAuth();
+
   const location = useLocation();
   const history = useHistory();
   const redirected_url = location?.state?.from || "/home";
@@ -15,12 +16,30 @@ const Login = () => {
       })
       .catch((error) => console.log(error.message));
   };
+
   return (
-    <div>
-      <button onClick={handleLogin} className="p-2 text-white bg-cyan-600">
-        Google
-      </button>
-    </div>
+    <>
+      <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+        <div className="py-4 bg-cyan-50 space-y-8 flex flex-col items-center justify-center text-center">
+          <h1 className="font-satisfy text-3xl md:text-5xl  text-cyan-400">
+            Please Login{" "}
+          </h1>
+          <div>
+            <button
+              onClick={handleLogin}
+              className="p-2 text-white bg-cyan-600 w-80"
+            >
+              Google
+            </button>
+          </div>
+        </div>
+        <img
+          className="md:col-span-2"
+          src="https://i.ibb.co/HGswJv3/hill.jpg"
+          alt="slider"
+        />
+      </div>
+    </>
   );
 };
 
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      notFound
-      <br />
-      <Link to="/home">Back</Link>
+    <div className="w-7/12 mx-auto flex flex-col space-y-6 mb-4">
+      <img src="https://i.ibb.co/mG8pXd8/notFound.png" alt="notfound" />
+      <Link className="px-4 py-2 bg-cyan-400 text-center text-white" to="/home">
+        Go Home
+      </Link>
     </div>
   );
 };
