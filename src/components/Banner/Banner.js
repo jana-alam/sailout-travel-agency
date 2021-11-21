a0b0737f8b47c94490f0abe5ac64f890be076d63
jana-alam <janaalam2929@gmail.com>
2021-11-21 21:20:11 +0600

    Repo change to public

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
       {/* left side */}
       <div className="py-8 bg-cyan-50 flex flex-col items-center justify-center text-center">
         <h1 className="font-satisfy text-3xl md:text-5xl lg:text-7xl text-cyan-400">
-          Lets go now{" "}
+          Lets Go Now{" "}
         </h1>
         <h3 className=" mt-4 text-5xl md:text-6xl font-bold text-gray-900">
           Explore The World
